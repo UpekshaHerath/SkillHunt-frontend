f82651d52a9c641bb98c53653dbfabc59fd43914
UpekshaHerath <upekshadilshan000@gmail.com>
2024-11-26 22:01:39 +0530

    feat: navigation bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { ModeToggle } from "@/components/ModeToggle";
-
-
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <ModeToggle />
+      
     </div>
   );
 };
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./ModeToggle";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center">
+        <div className="mr-4 flex">
+          <Link className="mr-6 flex items-center space-x-2" href="/">
+            <span className="font-bold text-xl">SkillHunt</span>
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-end space-x-2">
+          <nav className="flex items-center space-x-2">
+            <ModeToggle />
+            <Button variant="ghost" asChild>
+              <Link href="/sign-in">Sign In</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/sign-up">Sign Up</Link>
+            </Button>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
