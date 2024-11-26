59b92749c72dc19cedac18017531ad89b6787a42
UpekshaHerath <upekshadilshan000@gmail.com>
2024-11-26 19:09:29 +0530

    feat: implement theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ModeToggle";
+
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <Button>Click me</Button>
+      <ModeToggle />
     </div>
   );
 };
